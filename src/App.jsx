9dacd89fa409e7f39522126812def690cd094ea7
SportsPerformancePro <mmandia1@yahoo.com>
2025-04-20 17:19:10 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import React from 'react';
-import AthleteProfile from './components/AthleteProfile';
-import VideoUpload from './components/VideoUpload';
-import PromptForm from './components/PromptForm';
-import './style.css';
-
 export default function App() {
-  return (
-    <div className="container">
-      <h1>XLevel Coach</h1>
-      <AthleteProfile />
-      <VideoUpload />
-      <PromptForm />
-    </div>
-  );
+  return <h1>SPP ZIP 4d Final Ready</h1>;
 }
